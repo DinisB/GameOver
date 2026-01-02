9cd1a34ce22b63b0ea08972baef35f658631d69e
DinisB <dgamecreator@gmail.com>
2026-01-02 11:57:47 +0000

    Shop script done

---
+++ Assets/Scripts/UI/Menus/ShopScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToActivate;
+    [SerializeField] private GameObject shopUI;
+    private PlayerInventory playerInventory;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerInventory = FindFirstObjectByType<PlayerInventory>().GetComponent<PlayerInventory>();
+    }
+
+    public void BuyItem()
+    {
+        if (playerInventory.GetCoins() > 0)
+        {
+            playerInventory.SpendCoin();
+            FindFirstObjectByType<UIManager>().RefreshCoins();
+            objectToActivate.SetActive(true);
+            gameObject.SetActive(false);
+            shopUI.SetActive(false);
+        }
+    }
+}
